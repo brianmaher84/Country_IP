1d3e084b67818dc705c68f6f9f2c6db4724f8b97
Brian Maher <brianmaher@brians-mbp.home>
2016-05-22 13:15:51 +0100

    filter data to interesting attributes from web service

--- popup.js
+++ popup.js
@@ -1,5 +1,28 @@
 /*globals $, chrome, console, Uint8Array */
 
+function filterData(data) {
+    'use strict';
+    
+    var interestingKeys, filteredData;
+    
+    interestingKeys = ["city", "country", "countryCode", "isp", "org", "lat", "lon", "query", "regionName", "timezone", "zip"];
+    filteredData = {};
+    
+    $.each(data, function (k, v) {
+        
+        if (interestingKeys.indexOf(k) !== -1) {
+            if (k === 'query') {
+                k = 'IP';
+            } else if (k === 'org') {
+                k = 'organization';
+            }
+            filteredData[k] = v;
+        }
+        
+    });
+    return filteredData;
+}
+
 /**
  * creates the table data and updates the extension icon
  */
@@ -15,6 +38,8 @@ function createPopUpPage() {
         // https://www.whatismyip.com/images/flags/se.png
         img = "<img id=\"flagImg\" src=\"https://www.whatismyip.com/images/flags/" + data.countryCode.toLowerCase() + ".png\"/>";
 
+        data = filterData(data);
+        
         table_body = img;
         $.each(data, function (k, v) {
             table_body += "<tr><td>" + k + "</td><td><b>" + v + "</b></td></tr>";
@@ -22,7 +47,7 @@ function createPopUpPage() {
         $("#GeoResults").html(table_body);
 
         updateExtensionIcon(dataBlob);
-        
+
         initMapInternal(data.lat, data.lon);
     });
 }
